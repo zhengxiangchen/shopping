94e5458843596b51790da3094e4d943a9c6179b3
Jarvan <18998299214@189.cn>
2018-09-30 19:28:53 +0800

    0930
    冷启动自动更新

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -4,6 +4,26 @@ App({
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function () {
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+    })
+
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+
     if (!wx.cloud) {
       wx.showToast({
         title: '请更新微信版本',
